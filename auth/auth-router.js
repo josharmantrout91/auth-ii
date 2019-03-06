6f4557fe0fa5784b0f88725ab7fa58cddb60679d
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-06 17:38:34 -0500

    Successful POST to register new user

---
+++ auth/auth-router.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+const Users = require("../users/users-model");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
